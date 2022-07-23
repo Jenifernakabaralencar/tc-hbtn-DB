f763ea7a613d222d0e48c8f434c68493a8053a52
Jenifer <jnakabar@ciandt.com>
2022-07-22 22:38:15 -0300

    adicionando task 4

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE "CLIENTE" (
+"id"INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+"nome"varchar(80) NOT NULL,
+"email"varchar(40) NOT NULL,
+"telefone"INTEGER
+);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
